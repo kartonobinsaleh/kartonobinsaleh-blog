96b25d9ca3a03432c084c770f95b517d468fc9ce
kartonobinsaleh <kartono.saleh@gmail.com>
2025-12-10 16:46:54 +0700

    chore: update home page layout for combine hero & latest posts

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -26,13 +26,14 @@ export default async function HomePage() {
       <SectionContainer>
         <div className="divide-y divide-gray-200 dark:divide-gray-700">
           <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-            <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
-              Latest
+            <h1 className="text-2xl leading-7 font-bold tracking-tight text-gray-900 sm:text-3xl md:text-4xl dark:text-gray-100">
+              Apa yang baru?
             </h1>
-            <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            <p className="text-base leading-6 text-gray-500 dark:text-gray-400">
               {siteMetadata.description}
             </p>
           </div>
+
           <ul className="divide-y divide-gray-200 dark:divide-gray-700">
             {!posts.length && 'No posts found.'}
             {posts.slice(0, MAX_DISPLAY).map((post) => {
