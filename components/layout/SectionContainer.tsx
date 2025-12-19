ff28da29252a4e21963964a19ca636b3fc8a21c5
kartonobinsaleh <kartono.saleh@gmail.com>
2025-12-19 21:32:13 +0700

    chore: update py

--- components/layout/SectionContainer.tsx
+++ components/layout/SectionContainer.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function SectionContainer({ children, id }: Props) {
   return (
-    <section id={id} className="mx-auto max-w-7xl scroll-mt-20 px-4 py-12 sm:px-6 xl:px-0">
+    <section id={id} className="mx-auto max-w-7xl scroll-mt-20 px-4 py-4 sm:px-6 sm:py-12 xl:px-0">
       {children}
     </section>
   )
