2f9ff617ba1a89ba95c2e20654b58f0e4918e405
kartonobinsaleh <kartono.saleh@gmail.com>
2025-12-26 21:41:18 +0700

    feat(project): implement project card

--- app/(pages)/projects/page.tsx
+++ app/(pages)/projects/page.tsx
@@ -6,31 +6,35 @@ import { Metadata } from 'next'
 
 export const metadata: Metadata = genPageMetadata({ title: 'Projects' })
 
-export default function Projects() {
+export default function ProjectsPage() {
   return (
     <SectionContainer>
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
-            Projects
-          </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
-          </p>
-        </div>
-        <div className="container py-12">
-          <div className="-m-4 flex flex-wrap">
-            {projectsData.map((d) => (
+      <div className="pt-6">
+        <h1 className="mb-2 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl dark:text-gray-100">
+          Projects
+        </h1>
+
+        <p className="mb-8 text-lg leading-7 text-gray-500 dark:text-gray-400">
+          Showcase your projects with a hero image (16 x 9)
+        </p>
+
+        <ul className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {projectsData.map((project) => (
+            <li key={project.slug}>
               <ProjectCard
-                key={d.title}
-                title={d.title}
-                description={d.description}
-                imgSrc={d.imgSrc}
-                href={d.href}
+                title={project.title}
+                summary={project.summary}
+                slug={project.slug}
+                coverImage={project.coverImage}
+                href={project.href}
+                demoUrl={project.demoUrl}
+                githubUrl={project.githubUrl}
+                tech={project.tech}
+                status={project.status}
               />
-            ))}
-          </div>
-        </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </SectionContainer>
   )
